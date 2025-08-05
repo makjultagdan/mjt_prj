dd1454728a5b0722a476b3c93fc9b13ad9ffdc76
jaejoon <dlfhdlwm12@naver.com>
2025-08-05 20:59:37 +0900

    feat:북마크 추가 모달 생성

--- src/pages/BookmarkListPage.jsx
+++ src/pages/BookmarkListPage.jsx
@@ -1,9 +1,40 @@
+import { useState } from "react";
+import Sidebar from "../components/common/Sidebar";
+import AddBookmark from "../components/bookmark/AddBookmark";
 import BookmarkList from "../components/bookmark/BookmarkList.jsx";
 
 const BookmarkListPage = () => {
+  const [isAddBookmarkOpen, setIsAddBookmarkOpen] = useState(false);
+
+  const toggleAddBookmark = () => {
+    setIsAddBookmarkOpen(!isAddBookmarkOpen);
+  };
+
+  const closeAddBookmark = () => {
+    setIsAddBookmarkOpen(false);
+  };
+
   return (
-    <div>
-      <BookmarkList />
+    <div style={{ width: "100vw", height: "100vh", display: "flex" }}>
+      {/* 좌측 고정 Sidebar */}
+      <Sidebar />
+
+      {/* 메인 콘텐츠 영역 - 중앙 정렬 */}
+      <div
+        style={{
+          flex: 1,
+          height: "100vh",
+          overflow: "auto",
+          display: "flex",
+          justifyContent: "center",
+          padding: "2rem",
+        }}
+      >
+        <BookmarkList onToggleAddBookmark={toggleAddBookmark} />
+      </div>
+
+      {/* AddBookmark 모달 */}
+      <AddBookmark isOpen={isAddBookmarkOpen} onClose={closeAddBookmark} />
     </div>
   );
 };
